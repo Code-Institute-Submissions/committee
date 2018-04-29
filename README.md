ff935bc60d5191e20502f621b8d827bd54e5ff1f
sadfaerie <sadfaerie@gmail.com>
2018-04-29 14:49:24 +0100

    all commit after moving to local apache2 server

---
+++ README.md
@@ -0,0 +1,13 @@
+# committee
+
+A band tribute project.
+Visitors should be able to:
+- get information about the band and its members
+- listen to audio files
+- watch music videos 
+- get information about future events
+- contact the band via contact form and via links to social media sites. 
+
+Technologies used:
+HTML/HTML5
+CSS, Bootstrap
---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(function() {
+
+    // styling for nav bar, for fade effect on mouseover
+    $('.fadeElem').hover(function() {
+        // on mouseover, make current element visible
+      $(this).stop(true).fadeTo(200, 1); // duration, opacity
+    }, function() {
+        // on mouseout, fade current element
+      $(this).stop(true).fadeTo(300, .4);
+    });
+
+
+    // slider - toggle slide down and up on click
+    // $(".slider").click(function(){
+    //         $(".slideToggle").slideToggle("slow");
+    // });
+
+    // slider for lyrics
+    // hide .lyrics elements
+    $(".lyrics").hide('slow');
+    $(".title").click(function() {
+    // toggle child elements of current object
+    $(this).parent().children(".lyrics").slideToggle();
+    });
+
+
+    // slider for lyrics
+    // hide .lyrics elements
+    $(".slideToggle").hide('slow');
+    $(".slider").hover(function() {
+    // toggle child elements of current object
+    $(this).parent().children(".slideToggle").slideToggle();
+    });
+});
