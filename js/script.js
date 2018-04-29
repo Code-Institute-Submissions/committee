ff80d89fe3080bbfa7e04a30e0293657c455da7a
sadfaerie <sadfaerie@gmail.com>
2018-04-29 11:31:52 +0100

    added jquery slider, WIP

--- js/script.js
+++ js/script.js
@@ -1,9 +1,34 @@
 $(function() {
+
+    // styling for nav bar, for fade effect on mouseover
     $('.fadeElem').hover(function() {
-        // Code for Mouseover
+        // on mouseover, make current element visible
       $(this).stop(true).fadeTo(200, 1); // duration, opacity
     }, function() {
-        // Code for Mouseout
+        // on mouseout, fade current element
       $(this).stop(true).fadeTo(300, .4);
+    });
+
+
+    // slider - toggle slide down and up on click
+    // $(".slider").click(function(){
+    //         $(".slideToggle").slideToggle("slow");
+    // });
+
+    // slider for lyrics
+    // hide .lyrics elements
+    $(".lyrics").hide('slow');
+    $(".title").click(function() {
+    // toggle child elements of current object
+    $(this).parent().children(".lyrics").slideToggle();
+    });
+
+
+    // slider for lyrics
+    // hide .lyrics elements
+    $(".slideToggle").hide('slow');
+    $(".slider").click(function() {
+    // toggle child elements of current object
+    $(this).parent().children(".slideToggle").slideToggle();
     });
 });
