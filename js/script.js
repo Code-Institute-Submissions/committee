a0efd52ffe1af772bbcbcccf2abfb5bca17682ec
sadfaerie <sadfaerie@gmail.com>
2018-05-05 08:38:31 +0100

    modified homeBtn and smooth scrolling

--- js/script.js
+++ js/script.js
@@ -1,19 +1,14 @@
 $(function() {
+ var webpage = $('html, body'); // select html & body elements
 
     // styling for nav bar, for fade effect on mouseover
     $('.fadeElem').hover(function() {
         // on mouseover, make current element visible
-      $(this).stop(true).fadeTo(200, 1); // duration, opacity
+      $(this).stop(true).fadeTo(300, 1); // duration, opacity
     }, function() {
         // on mouseout, fade current element
-      $(this).stop(true).fadeTo(300, .4);
+      $(this).stop(true).fadeTo(300, .7);
     });
-
-
-    // slider - toggle slide down and up on click
-    // $(".slider").click(function(){
-    //         $(".slideToggle").slideToggle("slow");
-    // });
 
     // slider for lyrics
     // hide .lyrics elements
@@ -35,13 +30,27 @@ $(function() {
     // home button
     $(window).scroll(function(){
         if ($(this).scrollTop() > 90) {
-            $('#homeBtn').fadeIn();
+            $('#homeBtn').fadeIn(); // show homebutton when scrolled away from top
         } else {
             $('#homeBtn').fadeOut();
         }
     });
-    $('#homeBtn').click(function(){
-        $("html, body").animate({ scrollTop: 0 }, 600);
-        return false;
+    $('#homeBtn').on('click', function() {
+      evnt.preventDefault();
+      evnt.stopPropagation();
+        webpage.stop().animate({
+          scrollTop: 0
+        }, 2000, "easeInOutQuint");
     });
+
+
+    $(window).on('wheel', function() {
+      evnt.stopPropagation();
+      evnt.preventDefault();
+      var scrollAmt = evnt.originalEvent.wheelDelta > 0 ? '-=350' : '+=350';
+      webpage.stop().animate({
+        scrollTop:scrollAmt
+      },500,"linear");
+    });
+
 });
